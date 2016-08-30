77b0242d7f57eab66e4c31009e80c98fd343a57f
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-08-30 10:07:49 -0700

    Create wp-bootstrap-loader.php

---
+++ wp-bootstrap-loader.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* WP Bootstrap Loader
+*
+* @package wp-bootstrap-loader
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
+
+class WP_Bootstrap_Loader() {
+
+/**
+ * Loads Bootstrap & Font Awesome Stylesheet & Scripts.
+ *
+ * @access public
+ * @return void
+ */
+function bootstrap_scripts() {
+	if ( ! is_admin() ) {
+		// Font Awesome CSS.
+		wp_register_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', '', null, 'all' );
+		wp_enqueue_style( 'font-awesome' );
+		// Twitter Bootstrap CSS.
+		wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '', null, 'all' );
+		wp_enqueue_style( 'bootstrap' );
+		// Twitter Bootstrap Javascript.
+		wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', 'jquery', null, true );
+		wp_enqueue_script( 'bootstrap' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_scripts' );
+
+/**
+ * Loads Bootstrap & Font-Awesome CSS in WordPress editor.
+ *
+ * @access public
+ * @return void
+ */
+function bootstrap_editor_css() {
+
+		// Support Custom Editor CSS.
+		
+		add_editor_style( array(
+			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+			'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
+		));
+	}
+
+}
+
+}
