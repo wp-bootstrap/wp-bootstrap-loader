42be75eb8814313212fb16daa9cd23b9c40e564b
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-12-27 13:45:05 -0200

    Update wp-bootstrap-loader.php

--- wp-bootstrap-loader.php
+++ wp-bootstrap-loader.php
@@ -6,6 +6,7 @@
  */
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
+
 if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 
 	/**
@@ -25,8 +26,8 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 			add_action( 'wp_enqueue_scripts', array( $this, 'bootstrap_scripts' ) );
 
 			// Async & Defer.
-			add_filter('script_loader_tag', array( $this, 'bootstrap_async' ), 10, 2);
-			add_filter('script_loader_tag', array( $this, 'jquery_migrate_async' ), 10, 3);
+			add_filter( 'script_loader_tag', array( $this, 'bootstrap_async' ), 10, 2);
+			add_filter( 'script_loader_tag', array( $this, 'jquery_migrate_async' ), 10, 3);
 
 			// Load Bootstrap for WordPress Editor.
 			add_action( 'admin_init', array( $this, 'bootstrap_editor_css' ) );
@@ -40,9 +41,8 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 		 * @return void
 		 */
 		function bootstrap_async($tag, $handle) {
-			if ( 'bootstrap' !== $handle ) {
+			if ( 'bootstrap' !== $handle )
 				return $tag;
-			}
 			return str_replace( ' src', ' async="async" src', $tag );
 		}
 
@@ -55,9 +55,8 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 		 * @return void
 		 */
 		function jquery_migrate_async( $tag, $handle ) {
-			if ( 'jquery-migrate' !== $handle ) {
+			if ( 'jquery-migrate' !== $handle )
 				return $tag;
-			}
 			return str_replace( ' src', ' defer="defer" src', $tag );
 		}
 
@@ -69,22 +68,17 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 		 */
 		function bootstrap_scripts() {
 			if ( ! is_admin() ) {
-				
 				// Twitter Bootstrap CSS.
 				wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '', null, 'all' );
 				wp_enqueue_style( 'bootstrap' );
-				
 				// Load jQuery.
 				wp_enqueue_script( 'jquery' );
-				
 				// Twitter Bootstrap Javascript.
 				wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', 'jquery', null, true );
 				wp_enqueue_script( 'bootstrap' );
-				
 				// Google Webfont.
 				wp_register_script( 'webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js', null, null, true );
 				wp_enqueue_script( 'webfont' );
-				
 				// Load Font Awesome via Google Webfont.
 				wp_add_inline_script( 'webfont', 'WebFont.load({custom:{families:["font-awesome"],urls:["https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]}});' );
 			}
