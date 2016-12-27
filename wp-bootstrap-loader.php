00a6b12a3671fe3f09316bd1d901050994a4a102
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-12-27 12:43:31 -0200

    Update wp-bootstrap-loader.php

--- wp-bootstrap-loader.php
+++ wp-bootstrap-loader.php
@@ -4,17 +4,14 @@
  *
  * @package wp-bootstrap-loader
  */
-
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
-
 	/**
 	 * WP_Bootstrap_Loader class.
 	 */
 	class WP_Bootstrap_Loader {
-
 		/**
 		 * Construct
 		 *
@@ -26,10 +23,27 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 			// Load Bootstrap Scripts.
 			add_action( 'wp_enqueue_scripts', array( $this, 'bootstrap_scripts' ) );
 
+			// Async & Defer.
+			add_filter('script_loader_tag', array( $this, 'bootstrap_async' ), 10, 2);
+
 			// Load Bootstrap for WordPress Editor.
 			add_action( 'admin_init', array( $this, 'bootstrap_editor_css' ) );
 		}
 
+		/**
+		 * Bootstrap Async and Defer.
+		 *
+		 * @access public
+		 * @param mixed $tag Tag.
+		 * @param mixed $handle Handle.
+		 * @return void
+		 */
+		function bootstrap_async($tag, $handle) {
+			if ( 'bootstrap' !== $handle )
+				return $tag;
+
+			return str_replace( ' src', ' async="async" defer="defer" src', $tag );
+		}
 
 		/**
 		 * Loads Bootstrap & Font Awesome Stylesheet & Scripts.
@@ -39,28 +53,22 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 		 */
 		function bootstrap_scripts() {
 			if ( ! is_admin() ) {
-
 				// Twitter Bootstrap CSS.
 				wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', '', null, 'all' );
 				wp_enqueue_style( 'bootstrap' );
-				
+
 				// Load jQuery.
 				wp_enqueue_script( 'jquery' );
-
 				// Twitter Bootstrap Javascript.
 				wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', 'jquery', null, true );
 				wp_enqueue_script( 'bootstrap' );
-
 				// Google Webfont.
 				wp_register_script( 'webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js', null, null, true );
 				wp_enqueue_script( 'webfont' );
-
 				// Load Font Awesome via Google Webfont.
 				wp_add_inline_script( 'webfont', 'WebFont.load({custom:{families:["font-awesome"],urls:["https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]}});' );
-
 			}
 		}
-
 		/**
 		 * Loads Bootstrap & Font-Awesome CSS in WordPress editor.
 		 *
@@ -68,7 +76,6 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 		 * @return void
 		 */
 		function bootstrap_editor_css() {
-
 				// Support Custom Editor CSS.
 				add_editor_style( array(
 					'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
@@ -76,8 +83,5 @@ if ( ! class_exists( 'WP_Bootstrap_Loader' ) ) {
 				));
 		}
 	}
-
 }
-
-
-	new WP_Bootstrap_Loader;
+new WP_Bootstrap_Loader;
